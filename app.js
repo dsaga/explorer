017b444db87658654f6434716f3a04d530c08836
Dsaga <dulesaga@gmail.com>
2018-03-29 01:17:52 +0200

    remove code that we don't need

--- app.js
+++ app.js
@@ -39,16 +39,7 @@ if (settings.heavy != true) {
     'getmaxvote', 'getphase', 'getreward', 'getnextrewardestimate', 'getnextrewardwhenstr',
     'getnextrewardwhensec', 'getsupply', 'gettxoutsetinfo']);
 }
-// // view engine setup
-// app.set('views', path.join(__dirname, 'views'));
-// app.set('view engine', 'jade');
-//
-// app.use(favicon(path.join(__dirname, settings.favicon)));
-// app.use(logger('dev'));
-// app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded());
-// app.use(cookieParser());
-// app.use(express.static(path.join(__dirname, 'public')));
+
 
 // routes
 app.use('/api', bitcoinapi.app);
@@ -108,52 +99,5 @@ app.use('/ext/connections', function(req,res){
   });
 });
 //
-// // locals
-// app.set('title', settings.title);
-// app.set('symbol', settings.symbol);
-// app.set('coin', settings.coin);
-// app.set('locale', locale);
-// app.set('display', settings.display);
-// app.set('markets', settings.markets);
-// app.set('twitter', settings.twitter);
-// app.set('genesis_block', settings.genesis_block);
-// app.set('index', settings.index);
-// app.set('heavy', settings.heavy);
-// app.set('txcount', settings.txcount);
-// app.set('nethash', settings.nethash);
-// app.set('nethash_units', settings.nethash_units);
-// app.set('show_sent_received', settings.show_sent_received);
-// app.set('logo', settings.logo);
-// app.set('theme', settings.theme);
-// app.set('labels', settings.labels);
-
-// // catch 404 and forward to error handler
-// app.use(function(req, res, next) {
-//     var err = new Error('Not Found');
-//     err.status = 404;
-//     next(err);
-// });
-
-// development error handler
-// will print stacktrace
-// if (app.get('env') === 'development') {
-//     app.use(function(err, req, res, next) {
-//         res.status(err.status || 500);
-//         res.render('error', {
-//             message: err.message,
-//             error: err
-//         });
-//     });
-// }
-
-// production error handler
-// no stacktraces leaked to user
-// app.use(function(err, req, res, next) {
-//     res.status(err.status || 500);
-//     res.render('error', {
-//         message: err.message,
-//         error: {}
-//     });
-// });
 
 module.exports = app;
