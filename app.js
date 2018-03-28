451bc4fd9f87414c7044809894dca49ed1426a71
Dsaga <dulesaga@gmail.com>
2018-03-29 00:40:07 +0200

    test out block explorer without any view logic for explorer (just enable full api)

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 var express = require('express')
   , path = require('path')
   , bitcoinapi_class = require('bitcoin-node-api')
-  , favicon = require('static-favicon')
-  , logger = require('morgan')
+  , favicon = require('static-favicon') // not needed
+  , logger = require('morgan') // not needed
   , cookieParser = require('cookie-parser')
   , bodyParser = require('body-parser')
   , settings = require('./lib/settings')
-  , routes = require('./routes/index')
+  , routes = require('./routes/index') // not needed
   , lib = require('./lib/explorer')
   , db = require('./lib/database')
-  , locale = require('./lib/locale')
+  , locale = require('./lib/locale') // not needed
   , request = require('request');
 
 var app = express();
@@ -39,20 +39,20 @@ if (settings.heavy != true) {
     'getmaxvote', 'getphase', 'getreward', 'getnextrewardestimate', 'getnextrewardwhenstr',
     'getnextrewardwhensec', 'getsupply', 'gettxoutsetinfo']);
 }
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
-app.use(favicon(path.join(__dirname, settings.favicon)));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded());
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+// // view engine setup
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'jade');
+//
+// app.use(favicon(path.join(__dirname, settings.favicon)));
+// app.use(logger('dev'));
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded());
+// app.use(cookieParser());
+// app.use(express.static(path.join(__dirname, 'public')));
 
 // routes
 app.use('/api', bitcoinapi.app);
-app.use('/', routes);
+// app.use('/', routes);
 app.use('/ext/getmoneysupply', function(req,res){
   lib.get_supply(function(supply){
     res.send(' '+supply);
@@ -107,53 +107,53 @@ app.use('/ext/connections', function(req,res){
     res.send({data: peers});
   });
 });
+//
+// // locals
+// app.set('title', settings.title);
+// app.set('symbol', settings.symbol);
+// app.set('coin', settings.coin);
+// app.set('locale', locale);
+// app.set('display', settings.display);
+// app.set('markets', settings.markets);
+// app.set('twitter', settings.twitter);
+// app.set('genesis_block', settings.genesis_block);
+// app.set('index', settings.index);
+// app.set('heavy', settings.heavy);
+// app.set('txcount', settings.txcount);
+// app.set('nethash', settings.nethash);
+// app.set('nethash_units', settings.nethash_units);
+// app.set('show_sent_received', settings.show_sent_received);
+// app.set('logo', settings.logo);
+// app.set('theme', settings.theme);
+// app.set('labels', settings.labels);
 
-// locals
-app.set('title', settings.title);
-app.set('symbol', settings.symbol);
-app.set('coin', settings.coin);
-app.set('locale', locale);
-app.set('display', settings.display);
-app.set('markets', settings.markets);
-app.set('twitter', settings.twitter);
-app.set('genesis_block', settings.genesis_block);
-app.set('index', settings.index);
-app.set('heavy', settings.heavy);
-app.set('txcount', settings.txcount);
-app.set('nethash', settings.nethash);
-app.set('nethash_units', settings.nethash_units);
-app.set('show_sent_received', settings.show_sent_received);
-app.set('logo', settings.logo);
-app.set('theme', settings.theme);
-app.set('labels', settings.labels);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
-});
+// // catch 404 and forward to error handler
+// app.use(function(req, res, next) {
+//     var err = new Error('Not Found');
+//     err.status = 404;
+//     next(err);
+// });
 
 // development error handler
 // will print stacktrace
-if (app.get('env') === 'development') {
-    app.use(function(err, req, res, next) {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: err
-        });
-    });
-}
+// if (app.get('env') === 'development') {
+//     app.use(function(err, req, res, next) {
+//         res.status(err.status || 500);
+//         res.render('error', {
+//             message: err.message,
+//             error: err
+//         });
+//     });
+// }
 
 // production error handler
 // no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: {}
-    });
-});
+// app.use(function(err, req, res, next) {
+//     res.status(err.status || 500);
+//     res.render('error', {
+//         message: err.message,
+//         error: {}
+//     });
+// });
 
 module.exports = app;
